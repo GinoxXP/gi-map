f2422c2fcbb4a941e5ffb632945e992515f43cec
GinoxXP <seniorcartoche@gmail.com>
2025-11-25 21:02:00 +0300

    Refactoring

---
+++ GiMap/Client/AMapLayer.cs
@@ -0,0 +1,270 @@
+﻿using System.Collections.Concurrent;
+using System.Text;
+using GiMap.Config;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Datastructures;
+using Vintagestory.API.MathTools;
+using Vintagestory.GameContent;
+
+namespace GiMap.Client;
+
+public abstract class AMapLayer<Component> : MapLayer
+where Component : MultiChunkMapComponent
+{
+    public override EnumMapAppSide DataSide => EnumMapAppSide.Client;
+    
+    protected ICoreClientAPI _capi;
+    protected MapDB _mapdb;
+    protected IWorldChunk[] _chunksTmp;
+    protected const int _chunksize = 32;
+    protected float _mtThread1secAccum;
+    protected float _genAccum;
+    protected float _diskSaveAccum;
+    protected object _chunksToGenLock = new object();
+    protected UniqueQueue<FastVec2i> _chunksToGen = new UniqueQueue<FastVec2i>();
+    protected HashSet<FastVec2i> _curVisibleChunks = new HashSet<FastVec2i>();
+    protected ConcurrentQueue<ReadyMapPiece> _readyMapPieces = new ConcurrentQueue<ReadyMapPiece>();
+    protected Dictionary<FastVec2i, MapPieceDB> _toSaveList = new Dictionary<FastVec2i, MapPieceDB>();
+    protected ConcurrentDictionary<FastVec2i, Component> _loadedMapData = new ConcurrentDictionary<FastVec2i, Component>();
+    
+    public Vec4f OverlayColor { get; protected set; } = new Vec4f(1, 1, 1, 1);
+
+    protected AMapLayer(ICoreAPI api, IWorldMapManager mapSink) : base(api, mapSink)
+    {
+        OverlayColor.A = ConfigManager.ConfigInstance.overlayAlpha;
+        
+        api.Event.ChunkDirty += OnChunkDirty;
+        
+        Active = false;
+        
+        _capi = api as ICoreClientAPI;
+    }
+
+    public override void OnLoaded()
+    {
+        if (api.Side == EnumAppSide.Server)
+            return;
+
+        _chunksTmp = new IWorldChunk[api.World.BlockAccessor.MapSizeY / 32];
+    }
+    
+    public override void ComposeDialogExtras(GuiDialogWorldMap guiDialogWorldMap, GuiComposer compo)
+    {
+        string key = "worldmap-layer-" + LayerGroupCode;
+
+        ElementBounds dlgBounds =
+                ElementStdBounds.AutosizedMainDialog
+                    .WithFixedPosition(
+                        (compo.Bounds.renderX + compo.Bounds.OuterWidth) / RuntimeEnv.GUIScale + 10,
+                        (compo.Bounds.renderY + compo.Bounds.OuterHeight) / RuntimeEnv.GUIScale - 95
+                    )
+                    .WithAlignment(EnumDialogArea.None)
+            ;
+
+        ElementBounds row = ElementBounds.Fixed(0, 0, 160, 25);
+
+        ElementBounds bgBounds = ElementBounds.Fill.WithFixedPadding(GuiStyle.ElementToDialogPadding);
+        bgBounds.BothSizing = ElementSizing.FitToChildren;
+        bgBounds.WithChild(row);
+
+
+        guiDialogWorldMap.Composers[key] =
+            _capi.Gui
+                .CreateCompo(key, dlgBounds)
+                .AddShadedDialogBG(bgBounds, false)
+                .AddDialogTitleBar(Lang.Get("maplayer-"+LayerGroupCode), () => { guiDialogWorldMap.Composers[key].Enabled = false; })
+                .BeginChildElements(bgBounds)
+                .AddSlider((newValue) => { ConfigManager.ConfigInstance.overlayAlpha = newValue / 100.0f; OverlayColor.A = ConfigManager.ConfigInstance.overlayAlpha; return true; }, row = row.BelowCopy(0, 5).WithFixedSize(125, 25), "alpha-slider")
+                .EndChildElements()
+                .Compose()
+            ;
+
+        guiDialogWorldMap.Composers[key].GetSlider("alpha-slider").SetValues((int)(ConfigManager.ConfigInstance.overlayAlpha * 100), 0, 100, 1);
+
+        guiDialogWorldMap.Composers[key].Enabled = true;
+    }
+    
+    public override void OnMapClosedClient()
+    {
+        lock (_chunksToGenLock)
+            _chunksToGen.Clear();
+        
+        _curVisibleChunks.Clear();
+        ConfigManager.SaveModConfig(api);
+    }
+    
+    public override void Dispose()
+    {
+        if (_loadedMapData != null)
+        {
+            foreach (var value in _loadedMapData.Values)
+            {
+                value?.ActuallyDispose();
+            }
+        }
+
+        MultiChunkMapComponent.DisposeStatic();
+        base.Dispose();
+    }
+    
+    public override void OnShutDown()
+    {
+        MultiChunkMapComponent.tmpTexture?.Dispose();
+        _mapdb?.Dispose();
+    }
+    public override void OnMouseMoveClient(MouseEvent args, GuiElementMap mapElem, StringBuilder hoverText)
+    {
+    }
+
+    public override void OnMouseUpClient(MouseEvent args, GuiElementMap mapElem)
+    {
+    }
+    public override void OnOffThreadTick(float dt)
+    {
+        _genAccum += dt;
+        if (_genAccum < 0.1) return;
+        _genAccum = 0;
+
+        int quantityToGen = _chunksToGen.Count;
+        while (quantityToGen > 0)
+        {
+            if (mapSink.IsShuttingDown) break;
+
+            quantityToGen--;
+            FastVec2i cord;
+
+            lock (_chunksToGenLock)
+            {
+                if (_chunksToGen.Count == 0) break;
+                cord = _chunksToGen.Dequeue();
+            }
+
+            if (!api.World.BlockAccessor.IsValidPos(cord.X * _chunksize, 1, cord.Y * _chunksize)) continue;
+
+            IMapChunk mc = api.World.BlockAccessor.GetMapChunk(cord.X, cord.Y);
+            if (mc == null)
+            {
+                try
+                {
+                    MapPieceDB piece = _mapdb.GetMapPiece(cord);
+                    if (piece?.Pixels != null)
+                    {
+                        LoadFromChunkPixels(cord, piece.Pixels);
+                    }
+                }
+                catch (ProtoBuf.ProtoException)
+                {
+                    api.Logger.Warning("Failed loading map db section {0}/{1}, a protobuf exception was thrown. Will ignore.", cord.X, cord.Y);
+                }
+                catch (OverflowException)
+                {
+                    api.Logger.Warning("Failed loading map db section {0}/{1}, a overflow exception was thrown. Will ignore.", cord.X, cord.Y);
+                }
+
+                continue;
+            }
+
+            int[] tintedPixels = GenerateChunkImage(cord, mc);
+            if (tintedPixels == null)
+            {
+                lock (_chunksToGenLock)
+                {
+                    _chunksToGen.Enqueue(cord);
+                }
+
+                continue;
+            }
+
+            _toSaveList[cord.Copy()] = new MapPieceDB() { Pixels = tintedPixels };
+
+            LoadFromChunkPixels(cord, tintedPixels);
+        }
+
+        if (_toSaveList.Count > 100 || _diskSaveAccum > 4f)
+        {
+            _diskSaveAccum = 0;
+            _mapdb.SetMapPieces(_toSaveList);
+            _toSaveList.Clear();
+        }
+    }
+    
+    public override void Render(GuiElementMap mapElem, float dt)
+    {
+        if (!Active)
+        {
+            return;
+        }
+
+        foreach (KeyValuePair<FastVec2i, Component> loadedMapDatum in _loadedMapData)
+        {
+            loadedMapDatum.Value.Render(mapElem, dt);
+        }
+    }
+    
+    public override void OnViewChangedClient(List<FastVec2i> nowVisible, List<FastVec2i> nowHidden)
+    {
+        foreach (var val in nowVisible)
+        {
+            _curVisibleChunks.Add(val);
+        }
+
+        foreach (var val in nowHidden)
+        {
+            _curVisibleChunks.Remove(val);
+        }
+
+        lock (_chunksToGenLock)
+        {
+            foreach (FastVec2i cord in nowVisible)
+            {
+                FastVec2i tmpMccoord = new FastVec2i(cord.X / MultiChunkMapComponent.ChunkLen, cord.Y / MultiChunkMapComponent.ChunkLen);
+
+                int dx = cord.X % MultiChunkMapComponent.ChunkLen;
+                int dz = cord.Y % MultiChunkMapComponent.ChunkLen;
+                if (dx < 0 || dz < 0) continue;
+
+                if (_loadedMapData.TryGetValue(tmpMccoord, out Component mcomp))
+                {
+                    if (mcomp.IsChunkSet(dx, dz)) continue;
+                }
+
+                _chunksToGen.Enqueue(cord.Copy());
+            }
+        }
+
+        foreach (FastVec2i cord in nowHidden)
+        {
+            if (cord.X < 0 || cord.Y < 0) continue;
+
+            FastVec2i mcord = new FastVec2i(cord.X / MultiChunkMapComponent.ChunkLen, cord.Y / MultiChunkMapComponent.ChunkLen);
+
+            if (_loadedMapData.TryGetValue(mcord, out Component mc))
+            {
+                mc.unsetChunk(cord.X % MultiChunkMapComponent.ChunkLen, cord.Y % MultiChunkMapComponent.ChunkLen);
+            }
+        }
+    }
+
+    protected abstract void LoadFromChunkPixels(FastVec2i cord, int[] pixels);
+    
+    protected abstract int[] GenerateChunkImage(FastVec2i chunkPos, IMapChunk mc);
+    
+    private void OnChunkDirty(Vec3i chunkCoord, IWorldChunk chunk, EnumChunkDirtyReason reason)
+    {
+        lock (_chunksToGenLock)
+        {
+            if (!mapSink.IsOpened)
+                return;
+
+            var tmpMccoord = new FastVec2i(chunkCoord.X / MultiChunkMapComponent.ChunkLen, chunkCoord.Z / MultiChunkMapComponent.ChunkLen);
+            var tmpCoord = new FastVec2i(chunkCoord.X, chunkCoord.Z);
+
+            if (!_loadedMapData.ContainsKey(tmpMccoord) && !_curVisibleChunks.Contains(tmpCoord))
+                return;
+
+            _chunksToGen.Enqueue(new FastVec2i(chunkCoord.X, chunkCoord.Z));
+        }
+    }
+}
--- GiMap/Client/TopographicMapLayer.cs
+++ GiMap/Client/TopographicMapLayer.cs
@@ -1,37 +1,16 @@
-﻿using System.Collections.Concurrent;
-using System.Text;
-using GiMap.Config;
-using Vintagestory.API.Client;
+﻿using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Config;
-using Vintagestory.API.Datastructures;
 using Vintagestory.API.MathTools;
 using Vintagestory.GameContent;
 using Path = System.IO.Path;
 
 namespace GiMap.Client;
 
-public class TopographicMapLayer : MapLayer
+public class TopographicMapLayer : AMapLayer<TopographicMultiChunkMapComponent>
 {
     public override string Title => "topographic";
     public override string LayerGroupCode => "topographic";
-    public override EnumMapAppSide DataSide => EnumMapAppSide.Client;
-    
-    public Vec4f OverlayColor = new Vec4f(1, 1, 1, 1);
-
-    private ICoreClientAPI capi;
-    private MapDB mapdb;
-    private IWorldChunk[] chunksTmp;
-    private const int chunksize = 32;
-    private float mtThread1secAccum;
-    private float genAccum;
-    private float diskSaveAccum;
-    private object chunksToGenLock = new object();
-    private UniqueQueue<FastVec2i> chunksToGen = new UniqueQueue<FastVec2i>();
-    private HashSet<FastVec2i> curVisibleChunks = new HashSet<FastVec2i>();
-    private ConcurrentQueue<ReadyMapPiece> readyMapPieces = new ConcurrentQueue<ReadyMapPiece>();
-    private Dictionary<FastVec2i, MapPieceDB> toSaveList = new Dictionary<FastVec2i, MapPieceDB>();
-    private ConcurrentDictionary<FastVec2i, TopographicMultiChunkMapComponent> loadedMapData = new ConcurrentDictionary<FastVec2i, TopographicMultiChunkMapComponent>();
     
     private readonly int soilColor = ColorUtil.ColorFromRgba(201, 234, 157, 255);
     private readonly int sandColor = ColorUtil.ColorFromRgba(255, 255, 255, 255);
@@ -44,28 +23,20 @@ public class TopographicMapLayer : MapLayer
     
     public TopographicMapLayer(ICoreAPI api, IWorldMapManager mapSink) : base(api, mapSink)
     {
-        OverlayColor.A = ConfigManager.ConfigInstance.overlayAlpha;
-
-        api.Event.ChunkDirty += OnChunkDirty;
-
-        this.Active = false;
-        capi = api as ICoreClientAPI;
         if (api.Side == EnumAppSide.Client)
         {
             api.World.Logger.Notification("Loading world map cache db...");
-            mapdb = new MapDB(api.World.Logger);
+            _mapdb = new MapDB(api.World.Logger);
             string errorMessage = null;
             string mapDbFilePath = GetMapDbFilePath();
-            mapdb.OpenOrCreate(mapDbFilePath, ref errorMessage, requireWriteAccess: true, corruptionProtection: true, doIntegrityCheck: false);
+            _mapdb.OpenOrCreate(mapDbFilePath, ref errorMessage, requireWriteAccess: true, corruptionProtection: true, doIntegrityCheck: false);
             if (errorMessage != null)
-            {
                 throw new Exception($"Cannot open {mapDbFilePath}, possibly corrupted. Please fix manually or delete this file to continue playing");
-            }
 
-            api.ChatCommands.GetOrCreate("geomap").BeginSubCommand("purgedb").WithDescription("purge the map db")
+            api.ChatCommands.GetOrCreate("topographicmap").BeginSubCommand("purgedb").WithDescription("purge the map db")
                 .HandleWith(delegate
                 {
-                    mapdb.Purge();
+                    _mapdb.Purge();
                     return TextCommandResult.Success("Ok, db purged");
                 })
                 .EndSubCommand()
@@ -75,171 +46,23 @@ public class TopographicMapLayer : MapLayer
                 .EndSubCommand();
         }
     }
-    
-    public override void OnLoaded()
-    {
-        if (api.Side == EnumAppSide.Server)
-        {
-            return;
-        }
-
-        chunksTmp = new IWorldChunk[api.World.BlockAccessor.MapSizeY / 32];
-    }
-
-    public override void ComposeDialogExtras(GuiDialogWorldMap guiDialogWorldMap, GuiComposer compo)
-    {
-        string key = "worldmap-layer-" + LayerGroupCode;
-
-        ElementBounds dlgBounds =
-            ElementStdBounds.AutosizedMainDialog
-            .WithFixedPosition(
-                (compo.Bounds.renderX + compo.Bounds.OuterWidth) / RuntimeEnv.GUIScale + 10,
-                (compo.Bounds.renderY + compo.Bounds.OuterHeight) / RuntimeEnv.GUIScale - 95
-            )
-            .WithAlignment(EnumDialogArea.None)
-        ;
-
-        ElementBounds row = ElementBounds.Fixed(0, 0, 160, 25);
-
-        ElementBounds bgBounds = ElementBounds.Fill.WithFixedPadding(GuiStyle.ElementToDialogPadding);
-        bgBounds.BothSizing = ElementSizing.FitToChildren;
-        bgBounds.WithChild(row);
-
-
-        guiDialogWorldMap.Composers[key] =
-            capi.Gui
-                .CreateCompo(key, dlgBounds)
-                .AddShadedDialogBG(bgBounds, false)
-                .AddDialogTitleBar(Lang.Get("maplayer-"+LayerGroupCode), () => { guiDialogWorldMap.Composers[key].Enabled = false; })
-                .BeginChildElements(bgBounds)
-                    .AddSlider((newValue) => { ConfigManager.ConfigInstance.overlayAlpha = newValue / 100.0f; OverlayColor.A = ConfigManager.ConfigInstance.overlayAlpha; return true; }, row = row.BelowCopy(0, 5).WithFixedSize(125, 25), "alpha-slider")
-                .EndChildElements()
-                .Compose()
-        ;
-
-        guiDialogWorldMap.Composers[key].GetSlider("alpha-slider").SetValues((int)(ConfigManager.ConfigInstance.overlayAlpha * 100), 0, 100, 1);
-
-        guiDialogWorldMap.Composers[key].Enabled = true;
-    }
-
-    public override void OnMapClosedClient()
-    {
-        lock (chunksToGenLock)
-        {
-            chunksToGen.Clear();
-        }
-
-        curVisibleChunks.Clear();
-
-        ConfigManager.SaveModConfig(api);
-    }
-
-    public override void Dispose()
-    {
-        if (loadedMapData != null)
-        {
-            foreach (TopographicMultiChunkMapComponent value in loadedMapData.Values)
-            {
-                value?.ActuallyDispose();
-            }
-        }
-
-        MultiChunkMapComponent.DisposeStatic();
-        base.Dispose();
-    }
-
-    public override void OnShutDown()
-    {
-        MultiChunkMapComponent.tmpTexture?.Dispose();
-        mapdb?.Dispose();
-    }
-    
-    public override void OnOffThreadTick(float dt)
-    {
-        genAccum += dt;
-        if (genAccum < 0.1) return;
-        genAccum = 0;
-
-        int quantityToGen = chunksToGen.Count;
-        while (quantityToGen > 0)
-        {
-            if (mapSink.IsShuttingDown) break;
-
-            quantityToGen--;
-            FastVec2i cord;
-
-            lock (chunksToGenLock)
-            {
-                if (chunksToGen.Count == 0) break;
-                cord = chunksToGen.Dequeue();
-            }
-
-            if (!api.World.BlockAccessor.IsValidPos(cord.X * chunksize, 1, cord.Y * chunksize)) continue;
-
-            IMapChunk mc = api.World.BlockAccessor.GetMapChunk(cord.X, cord.Y);
-            if (mc == null)
-            {
-                try
-                {
-                    MapPieceDB piece = mapdb.GetMapPiece(cord);
-                    if (piece?.Pixels != null)
-                    {
-                        LoadFromChunkPixels(cord, piece.Pixels);
-                    }
-                }
-                catch (ProtoBuf.ProtoException)
-                {
-                    api.Logger.Warning("Failed loading map db section {0}/{1}, a protobuf exception was thrown. Will ignore.", cord.X, cord.Y);
-                }
-                catch (OverflowException)
-                {
-                    api.Logger.Warning("Failed loading map db section {0}/{1}, a overflow exception was thrown. Will ignore.", cord.X, cord.Y);
-                }
-
-                continue;
-            }
-
-            int[] tintedPixels = GenerateChunkImage(cord, mc);
-            if (tintedPixels == null)
-            {
-                lock (chunksToGenLock)
-                {
-                    chunksToGen.Enqueue(cord);
-                }
-
-                continue;
-            }
-
-            toSaveList[cord.Copy()] = new MapPieceDB() { Pixels = tintedPixels };
-
-            LoadFromChunkPixels(cord, tintedPixels);
-        }
-
-        if (toSaveList.Count > 100 || diskSaveAccum > 4f)
-        {
-            diskSaveAccum = 0;
-            mapdb.SetMapPieces(toSaveList);
-            toSaveList.Clear();
-        }
-    }
 
     public override void OnTick(float dt)
     {
-        if (!readyMapPieces.IsEmpty)
+        if (!_readyMapPieces.IsEmpty)
         {
-            int q = Math.Min(readyMapPieces.Count, 200);
+            int q = Math.Min(_readyMapPieces.Count, 200);
             List<MultiChunkMapComponent> modified = new();
             while (q-- > 0)
             {
-                if (readyMapPieces.TryDequeue(out var mappiece))
+                if (_readyMapPieces.TryDequeue(out var mappiece))
                 {
                     FastVec2i mcord = new FastVec2i(mappiece.Cord.X / MultiChunkMapComponent.ChunkLen, mappiece.Cord.Y / MultiChunkMapComponent.ChunkLen);
                     FastVec2i baseCord = new FastVec2i(mcord.X * MultiChunkMapComponent.ChunkLen, mcord.Y * MultiChunkMapComponent.ChunkLen);
 
-                    if (!loadedMapData.TryGetValue(mcord, out TopographicMultiChunkMapComponent mccomp))
-                    {
-                        loadedMapData[mcord] = mccomp = new TopographicMultiChunkMapComponent(api as ICoreClientAPI, baseCord, this);
-                    }
+                    if (!_loadedMapData.TryGetValue(mcord, out TopographicMultiChunkMapComponent mccomp))
+                        _loadedMapData[mcord] = mccomp = new TopographicMultiChunkMapComponent(api as ICoreClientAPI, baseCord, this);
+
 
                     mccomp.setChunk(mappiece.Cord.X - baseCord.X, mappiece.Cord.Y - baseCord.Y, mappiece.Pixels);
                     modified.Add(mccomp);
@@ -249,16 +72,16 @@ public class TopographicMapLayer : MapLayer
             foreach (var mccomp in modified) mccomp.FinishSetChunks();
         }
 
-        mtThread1secAccum += dt;
-        if (mtThread1secAccum > 1)
+        _mtThread1secAccum += dt;
+        if (_mtThread1secAccum > 1)
         {
             List<FastVec2i> toRemove = new List<FastVec2i>();
 
-            foreach (var val in loadedMapData)
+            foreach (var val in _loadedMapData)
             {
                 MultiChunkMapComponent mcmp = val.Value;
 
-                if (!mcmp.AnyChunkSet || !mcmp.IsVisible(curVisibleChunks))
+                if (!mcmp.AnyChunkSet || !mcmp.IsVisible(_curVisibleChunks))
                 {
                     mcmp.TTL -= 1;
 
@@ -276,76 +99,10 @@ public class TopographicMapLayer : MapLayer
             }
 
             foreach (var val in toRemove)
-            {
-                loadedMapData.TryRemove(val, out _);
-            }
+                _loadedMapData.TryRemove(val, out _);
 
-            mtThread1secAccum = 0;
-        }
-    }
 
-    public override void Render(GuiElementMap mapElem, float dt)
-    {
-        if (!base.Active)
-        {
-            return;
-        }
-
-        foreach (KeyValuePair<FastVec2i, TopographicMultiChunkMapComponent> loadedMapDatum in loadedMapData)
-        {
-            loadedMapDatum.Value.Render(mapElem, dt);
-        }
-    }
-
-    public override void OnMouseMoveClient(MouseEvent args, GuiElementMap mapElem, StringBuilder hoverText)
-    {
-    }
-
-    public override void OnMouseUpClient(MouseEvent args, GuiElementMap mapElem)
-    {
-    }
-    
-    public override void OnViewChangedClient(List<FastVec2i> nowVisible, List<FastVec2i> nowHidden)
-    {
-        foreach (var val in nowVisible)
-        {
-            curVisibleChunks.Add(val);
-        }
-
-        foreach (var val in nowHidden)
-        {
-            curVisibleChunks.Remove(val);
-        }
-
-        lock (chunksToGenLock)
-        {
-            foreach (FastVec2i cord in nowVisible)
-            {
-                FastVec2i tmpMccoord = new FastVec2i(cord.X / MultiChunkMapComponent.ChunkLen, cord.Y / MultiChunkMapComponent.ChunkLen);
-
-                int dx = cord.X % MultiChunkMapComponent.ChunkLen;
-                int dz = cord.Y % MultiChunkMapComponent.ChunkLen;
-                if (dx < 0 || dz < 0) continue;
-
-                if (loadedMapData.TryGetValue(tmpMccoord, out TopographicMultiChunkMapComponent mcomp))
-                {
-                    if (mcomp.IsChunkSet(dx, dz)) continue;
-                }
-
-                chunksToGen.Enqueue(cord.Copy());
-            }
-        }
-
-        foreach (FastVec2i cord in nowHidden)
-        {
-            if (cord.X < 0 || cord.Y < 0) continue;
-
-            FastVec2i mcord = new FastVec2i(cord.X / MultiChunkMapComponent.ChunkLen, cord.Y / MultiChunkMapComponent.ChunkLen);
-
-            if (loadedMapData.TryGetValue(mcord, out TopographicMultiChunkMapComponent mc))
-            {
-                mc.unsetChunk(cord.X % MultiChunkMapComponent.ChunkLen, cord.Y % MultiChunkMapComponent.ChunkLen);
-            }
+            _mtThread1secAccum = 0;
         }
     }
     
@@ -358,89 +115,68 @@ public class TopographicMapLayer : MapLayer
     
     private TextCommandResult OnMapCmdRedraw(TextCommandCallingArgs args)
     {
-        foreach (TopographicMultiChunkMapComponent value in loadedMapData.Values)
+        foreach (var value in _loadedMapData.Values)
         {
             value.ActuallyDispose();
         }
 
-        loadedMapData.Clear();
-        lock (chunksToGenLock)
+        _loadedMapData.Clear();
+        lock (_chunksToGenLock)
         {
-            foreach (FastVec2i curVisibleChunk in curVisibleChunks)
+            foreach (FastVec2i curVisibleChunk in _curVisibleChunks)
             {
-                chunksToGen.Enqueue(curVisibleChunk.Copy());
+                _chunksToGen.Enqueue(curVisibleChunk.Copy());
             }
         }
 
         return TextCommandResult.Success("Redrawing map...");
     }
-    
-    private void OnChunkDirty(Vec3i chunkCoord, IWorldChunk chunk, EnumChunkDirtyReason reason)
+
+    protected override void LoadFromChunkPixels(FastVec2i cord, int[] pixels)
     {
-        lock (chunksToGenLock)
-        {
-            if (!mapSink.IsOpened) return;
-
-            FastVec2i tmpMccoord = new FastVec2i(chunkCoord.X / MultiChunkMapComponent.ChunkLen, chunkCoord.Z / MultiChunkMapComponent.ChunkLen);
-            FastVec2i tmpCoord = new FastVec2i(chunkCoord.X, chunkCoord.Z);
-
-            if (!loadedMapData.ContainsKey(tmpMccoord) && !curVisibleChunks.Contains(tmpCoord)) return;
-
-            chunksToGen.Enqueue(new FastVec2i(chunkCoord.X, chunkCoord.Z));
-        }
-    }
-
-    private void LoadFromChunkPixels(FastVec2i cord, int[] pixels)
-    {
-        readyMapPieces.Enqueue(new ReadyMapPiece
+        _readyMapPieces.Enqueue(new ReadyMapPiece
         {
             Pixels = pixels,
             Cord = cord
         });
     }
     
-    private int[] GenerateChunkImage(FastVec2i chunkPos, IMapChunk mc)
+    protected override int[] GenerateChunkImage(FastVec2i chunkPos, IMapChunk mc)
     {
-        Vec2i vec2i = new Vec2i();
+        var vec2i = new Vec2i();
 
-        for (int i = 0; i < chunksTmp.Length; i++)
+        for (var i = 0; i < _chunksTmp.Length; i++)
         {
-            chunksTmp[i] = capi.World.BlockAccessor.GetChunk(chunkPos.X, i, chunkPos.Y);
-            if (chunksTmp[i] == null || !(chunksTmp[i] as IClientChunk).LoadedFromServer)
-            {
+            _chunksTmp[i] = _capi.World.BlockAccessor.GetChunk(chunkPos.X, i, chunkPos.Y);
+            if (_chunksTmp[i] == null || !(_chunksTmp[i] as IClientChunk).LoadedFromServer)
                 return null;
-            }
         }
 
-        int[] resultPixelArray = new int[1024];
-        for (int k = 0; k < resultPixelArray.Length; k++)
+        var resultPixelArray = new int[1024];
+        for (var k = 0; k < resultPixelArray.Length; k++)
         {
             int topBlockHeight = mc.RainHeightMap[k];
             int topChunkIndex = topBlockHeight / 32;
-            if (topChunkIndex >= chunksTmp.Length)
-            {
+            if (topChunkIndex >= _chunksTmp.Length)
                 continue;
-            }
             
             MapUtil.PosInt2d(k, 32L, vec2i);
-            int index = chunksTmp[topChunkIndex].UnpackAndReadBlock(MapUtil.Index3d(vec2i.X, topBlockHeight % 32, vec2i.Y, 32, 32), 3);
+            int index = _chunksTmp[topChunkIndex].UnpackAndReadBlock(MapUtil.Index3d(vec2i.X, topBlockHeight % 32, vec2i.Y, 32, 32), 3);
             Block block = api.World.Blocks[index];
             
             while (topBlockHeight > 0 && !IsBlockValid(block))
             {
                 topBlockHeight--;
                 topChunkIndex = topBlockHeight / 32;
-                index = chunksTmp[topChunkIndex].UnpackAndReadBlock(MapUtil.Index3d(vec2i.X, topBlockHeight % 32, vec2i.Y, 32, 32), 3);
+                index = _chunksTmp[topChunkIndex].UnpackAndReadBlock(MapUtil.Index3d(vec2i.X, topBlockHeight % 32, vec2i.Y, 32, 32), 3);
                 block = api.World.Blocks[index];
             }
 
             resultPixelArray[k] = GetMaterialColor(block);
         }
         
-        for (int n = 0; n < chunksTmp.Length; n++)
-        {
-            chunksTmp[n] = null;
-        }
+        for (var n = 0; n < _chunksTmp.Length; n++)
+            _chunksTmp[n] = null;
             
         return resultPixelArray;
     }
